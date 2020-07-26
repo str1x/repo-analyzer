e4309b22d11236421e7ef66871998ee1b4e78366
Max Green <strixdevelop@gmail.com>
2020-07-26 13:55:52 +0300

    get authors list

--- native/lib/git_analyzer/src/lib.rs
+++ native/lib/git_analyzer/src/lib.rs
@@ -1,26 +1,34 @@
 use git2;
 
-pub struct Repository {
-    path: String,
-    inner_repo: git2::Repository,
-}
-
-pub struct RepoGeneralData {
-    pub path: String,
-    pub files: Vec<String>,
-    pub contributors: Vec<String>,
-}
-
 pub enum Error {
     FailedToOpen,
+    RevwalkCommits,
 }
 
 impl AsRef<str> for Error {
     fn as_ref(&self) -> &str {
         match self {
             Error::FailedToOpen => "Repo not found",
+            Error::RevwalkCommits => "Failed to traverse the commit graph",
         }
     }
+}
+
+pub struct Repository {
+    path: String,
+    inner_repo: git2::Repository,
+}
+
+pub struct AuthorsItem {
+    pub name: String,
+    pub commits_count: u32,
+    pub email: String,
+}
+
+pub struct RepoGeneralData {
+    pub path: String,
+    pub files: Vec<String>,
+    pub authors: Vec<AuthorsItem>,
 }
 
 impl Repository {
@@ -46,24 +54,51 @@ impl Repository {
         return files;
     }
 
-    // pub fn get_contributors(&self) -> Vec<String> {
-    //     let index = self.inner_repo.index().unwrap();
-    //     let contributors = index
-    //         .iter()
-    //         .map(|item| {
-    //             String::from_utf8(item.path).unwrap()
-    //         })
+    pub fn get_authors(&self) -> Result<Vec<AuthorsItem>, Error> {
+        let mut revwalk = self.inner_repo.revwalk()
+            .map_err(|_| Error::RevwalkCommits)?;
+        revwalk.push_head()
+            .map_err(|_| Error::RevwalkCommits)?;
 
+        let mut authors = revwalk
+            .map(|r| {
+                let oid = r?;
+                self.inner_repo.find_commit(oid)
+            })
+            .filter_map(|c| match c {
+                Ok(commit) => Some(commit),
+                Err(_e) => None,
+            })
+            .fold(vec!(), |mut authors: Vec<AuthorsItem>, commit| {
+                if let Some(email) = commit.author().email() {
+                    let index = authors.iter().position(|x| x.email == email);
+                    if index.is_some() {
+                        let index = index.unwrap();
+                        authors[index].commits_count += 1;
+                    } else {
+                        authors.push(AuthorsItem {
+                            email: email.to_string(),
+                            name: commit.author().name().unwrap().to_string(),
+                            commits_count: 1,
+                        });
+                    }
+                }
+                authors
+            });
 
-    //     return contributors;
-    // }
+        authors.sort_by(|a, b| b.commits_count.cmp(&a.commits_count));
 
-    pub fn get_general_data(&self) -> RepoGeneralData {
-        return RepoGeneralData {
+        Ok(authors)
+    }
+
+    pub fn get_general_data(&self) -> Result<RepoGeneralData, Error> {
+        let general_data = RepoGeneralData {
             path: self.path.clone(),
             files: self.get_files(),
-            contributors: vec!(),
+            authors: self.get_authors()?,
         };
+        
+        Ok(general_data)
     }
 }
 
--- native/src/lib.rs
+++ native/src/lib.rs
@@ -14,16 +14,36 @@ fn open_repository(mut cx: FunctionContext) -> JsResult<JsObject> {
             let message = cx.string(e);
             cx.throw(message)
         })?;
-    let general_data = repo.get_general_data();
-    let object = JsObject::new(&mut cx);
+    let general_data = repo.get_general_data()
+        .or_else(|e| {
+            let message = cx.string(e);
+            cx.throw(message)
+        })?;
+
     let path_prop = cx.string(&general_data.path);
+
     let files_prop = JsArray::new(& mut cx, general_data.files.len() as u32);
     for (idx, path) in general_data.files.iter().enumerate() {
         let js_string = cx.string(path);
         files_prop.set(&mut cx, idx as u32, js_string).unwrap();
     }
+
+    let authors_prop = JsArray::new(& mut cx, general_data.authors.len() as u32);
+    for (idx, author) in general_data.authors.iter().enumerate() {
+        let author_email = cx.string(&author.email);
+        let author_name = cx.string(&author.name);
+        let author_commits_cnt = cx.number(author.commits_count);
+        let author_object = JsObject::new(&mut cx);
+        author_object.set(&mut cx, "email", author_email).unwrap();
+        author_object.set(&mut cx, "name", author_name).unwrap();
+        author_object.set(&mut cx, "commitsCount", author_commits_cnt).unwrap();
+        authors_prop.set(&mut cx, idx as u32, author_object).unwrap();
+    }
+
+    let object = JsObject::new(&mut cx);
     object.set(&mut cx, "path", path_prop).unwrap();
     object.set(&mut cx, "files", files_prop).unwrap();
+    object.set(&mut cx, "authors", authors_prop).unwrap();
     Ok(object)
 }
 
